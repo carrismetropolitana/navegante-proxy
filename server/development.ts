1365fc44f711ce99a2990fc2579b884641a34c84
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-24 14:11:36 +0100

    Cleanup

--- server/development.ts
+++ server/development.ts
@@ -4,7 +4,7 @@ import FASTIFY from '@/services/FASTIFY.js';
 
 /* * */
 
-const android = async (_, reply) => {
+FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 	return reply
 		.code(200)
 		.header('Content-Type', 'application/json; charset=utf-8')
@@ -12,21 +12,21 @@ const android = async (_, reply) => {
 			{
 				build_max: 1,
 				build_min: null,
-				message_id: 'ANDROID-0082',
+				message_id: 'DEV-ANDROID-0082',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-android/startup/message-1',
 				presentation_type: 'breaking',
 			},
 			{
 				build_max: 1,
 				build_min: 2,
-				message_id: 'ANDROID-0005',
+				message_id: 'DEV-ANDROID-0005',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-android/startup/message-1',
 				presentation_type: 'changelog',
 			},
 		]);
-};
+});
 
-const ios = async (_, reply) => {
+FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 	return reply
 		.code(200)
 		.header('Content-Type', 'application/json; charset=utf-8')
@@ -34,21 +34,16 @@ const ios = async (_, reply) => {
 			{
 				build_max: 1,
 				build_min: null,
-				message_id: 'IOS-0082',
+				message_id: 'DEV-IOS-0082',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-ios/startup/message-1',
 				presentation_type: 'breaking',
 			},
 			{
 				build_max: 1,
 				build_min: 2,
-				message_id: 'IOS-0005',
+				message_id: 'DEV-IOS-0005',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-ios/startup/message-1',
 				presentation_type: 'changelog',
 			},
 		]);
-};
-
-/* * */
-
-FASTIFY.server.get('/dev/android/v1/startup/message', android);
-FASTIFY.server.get('/dev/ios/v1/startup/message', ios);
+});
