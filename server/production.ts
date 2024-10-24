db8044803dc42910ef592798ce55abcc02ab7e6f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-24 14:30:50 +0100

    Update production.ts

--- server/production.ts
+++ server/production.ts
@@ -10,18 +10,11 @@ FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
 			{
-				build_max: 1,
+				build_max: 37,
 				build_min: null,
 				message_id: 'PROD-ANDROID-0082',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-android/startup/message-1',
 				presentation_type: 'breaking',
-			},
-			{
-				build_max: 1,
-				build_min: 2,
-				message_id: 'PROD-ANDROID-0005',
-				message_url: 'https://alpha.carrismetropolitana.pt/app-android/startup/message-1',
-				presentation_type: 'changelog',
 			},
 		]);
 });
@@ -32,7 +25,7 @@ FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
 			{
-				build_max: 1,
+				build_max: 34,
 				build_min: null,
 				message_id: 'PROD-IOS-0082',
 				message_url: 'https://alpha.carrismetropolitana.pt/app-ios/startup/message-1',
