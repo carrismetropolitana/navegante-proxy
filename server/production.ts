9a0c67e985ee7dc4017ee8889b66d8ebb4cd7a75
João de Vasconcelos <contact@joao.earth>
2025-01-27 11:59:19 +0000

    Update Android and iOS startup messages with new build values and message IDs

--- server/production.ts
+++ server/production.ts
@@ -10,11 +10,18 @@ FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
 			{
-				build_max: 1,
+				build_max: 41,
 				build_min: null,
-				message_id: 'PROD-ANDROID-0082',
-				message_url: 'https://cmet.pt/app-navegante-android/startup/default',
+				message_id: 'ANDROID-20250115',
+				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/20250115',
 				presentation_type: 'breaking',
+			},
+			{
+				build_max: null,
+				build_min: 42,
+				message_id: 'ANDROID-nao-aumenta-2025',
+				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
+				presentation_type: 'changelog',
 			},
 		]);
 });
@@ -25,11 +32,18 @@ FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
 			{
-				build_max: 2,
+				build_max: 38,
 				build_min: null,
-				message_id: 'PROD-IOS-0082',
-				message_url: 'https://cmet.pt/app-navegante-ios/startup/default',
+				message_id: 'IOS-20250115',
+				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/20250115',
 				presentation_type: 'breaking',
+			},
+			{
+				build_max: null,
+				build_min: 39,
+				message_id: 'IOS-nao-aumenta-2025',
+				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
+				presentation_type: 'changelog',
 			},
 		]);
 });
