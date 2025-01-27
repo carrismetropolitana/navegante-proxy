682f3cc322442c78aa72485f417551c2255b6d96
João de Vasconcelos <contact@joao.earth>
2025-01-27 10:38:43 +0000

    Change presentation type from 'breaking' to 'changelog' for Android and iOS startup messages in development.ts

--- server/development.ts
+++ server/development.ts
@@ -14,14 +14,14 @@ FASTIFY.server.get('/dev/android/v1/startup/message', async (_, reply) => {
 				build_min: null,
 				message_id: 'DEV-ANDROID-20250115',
 				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/20250115',
-				presentation_type: 'breaking',
+				presentation_type: 'changelog',
 			},
 			{
 				build_max: null,
 				build_min: 39,
 				message_id: 'DEV-ANDROID-nao-aumenta-2025',
 				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
-				presentation_type: 'breaking',
+				presentation_type: 'changelog',
 			},
 		]);
 });
@@ -36,14 +36,14 @@ FASTIFY.server.get('/dev/ios/v1/startup/message', async (_, reply) => {
 				build_min: null,
 				message_id: 'DEV-IOS-20250115',
 				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/20250115',
-				presentation_type: 'breaking',
+				presentation_type: 'changelog',
 			},
 			{
 				build_max: null,
 				build_min: 39,
 				message_id: 'DEV-IOS-nao-aumenta-2025',
 				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
-				presentation_type: 'breaking',
+				presentation_type: 'changelog',
 			},
 		]);
 });
