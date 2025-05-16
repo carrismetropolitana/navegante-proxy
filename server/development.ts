d48bbce0a1493d4263857eb178839a1618a06ff4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-16 18:13:03 +0100

    Update to new URLs

--- server/development.ts
+++ server/development.ts
@@ -13,14 +13,14 @@ FASTIFY.server.get('/dev/android/v1/startup/message', async (_, reply) => {
 				build_max: 41,
 				build_min: null,
 				message_id: 'DEV-ANDROID-20250115',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/20250115',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-android/startup/20250115',
 				presentation_type: 'breaking',
 			},
 			{
 				build_max: null,
 				build_min: 42,
 				message_id: 'DEV-ANDROID-nao-aumenta-2025',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
 				presentation_type: 'changelog',
 			},
 		]);
@@ -35,14 +35,14 @@ FASTIFY.server.get('/dev/ios/v1/startup/message', async (_, reply) => {
 				build_max: 38,
 				build_min: null,
 				message_id: 'DEV-IOS-20250115',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/20250115',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-ios/startup/20250115',
 				presentation_type: 'breaking',
 			},
 			{
 				build_max: null,
 				build_min: 39,
 				message_id: 'DEV-IOS-nao-aumenta-2025',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
 				presentation_type: 'changelog',
 			},
 		]);
--- server/production.ts
+++ server/production.ts
@@ -13,14 +13,14 @@ FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 				build_max: 41,
 				build_min: null,
 				message_id: 'ANDROID-20250115',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/20250115',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-android/startup/20250115',
 				presentation_type: 'breaking',
 			},
 			// {
 			// 	build_max: null,
 			// 	build_min: 42,
 			// 	message_id: 'ANDROID-nao-aumenta-2025',
-			// 	message_url: 'https://www.carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
+			// 	message_url: 'https://carrismetropolitana.pt/app-navegante-android/startup/nao-aumenta-2025',
 			// 	presentation_type: 'changelog',
 			// },
 		]);
@@ -35,14 +35,14 @@ FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 				build_max: 38,
 				build_min: null,
 				message_id: 'IOS-20250115',
-				message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/20250115',
+				message_url: 'https://carrismetropolitana.pt/app-navegante-ios/startup/20250115',
 				presentation_type: 'breaking',
 			},
 			// {
 			// 	build_max: null,
 			// 	build_min: 39,
 			// 	message_id: 'IOS-nao-aumenta-2025',
-			// 	message_url: 'https://www.carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
+			// 	message_url: 'https://carrismetropolitana.pt/app-navegante-ios/startup/nao-aumenta-2025',
 			// 	presentation_type: 'changelog',
 			// },
 		]);
