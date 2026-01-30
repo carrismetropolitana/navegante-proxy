3ea75019b30177e6b0d45ce540c04995f0f9d003
ricardo-lima-tml <ricardo.lima@tmlmobilidade.pt>
2026-01-30 11:58:34 +0000

    Removed the breaking changes from stg

--- server/development.ts
+++ server/development.ts
@@ -9,20 +9,13 @@ FASTIFY.server.get('/dev/android/v1/startup/message', async (_, reply) => {
 		.code(200)
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
-			{
+			/*{
 				build_max: 90,
 				build_min: null,
 				message_id: 'DEV-ANDROID-20260127',
 				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/20260127',
 				presentation_type: 'breaking',
-			},
-			{
-				build_max: 41,
-				build_min: null,
-				message_id: 'DEV-ANDROID-20250115',
-				message_url: 'https://carrismetropolitana.pt/app-navegante-android/startup/20250115',
-				presentation_type: 'breaking',
-			},
+			},*/
 			{
 				build_max: null,
 				build_min: 42,
@@ -38,13 +31,13 @@ FASTIFY.server.get('/dev/ios/v1/startup/message', async (_, reply) => {
 		.code(200)
 		.header('Content-Type', 'application/json; charset=utf-8')
 		.send([
-			{
+			/*{
 				build_max: 93,
 				build_min: null,
 				message_id: 'DEV-IOS-20260128',
 				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-ios/startup/20260127',
 				presentation_type: 'breaking',
-			},
+			},*/
 			{
 				build_max: null,
 				build_min: 39,
