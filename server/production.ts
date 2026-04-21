d1518b7b5e7cddd1a30541f15a56a5f794444656
Samanta Coutinho <samanta.coutinho@tmlmobilidade.pt>
2026-04-21 10:41:36 +0100

    Commented the changelog until ASI is updated.

--- server/production.ts
+++ server/production.ts
@@ -18,13 +18,13 @@ FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 				message_url: 'https://pcgi.tmlmobilidade.pt/app-navegante-android/startup/20260127',
 				presentation_type: 'breaking',
 			},
-			{
-				build_max: null,
-				build_min: 96,
-				message_id: 'navegante-rir-2026',
-				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/rock-in-rio-2026',
-				presentation_type: 'changelog',
-			},
+			// {
+			// 	build_max: null,
+			// 	build_min: 96,
+			// 	message_id: 'navegante-rir-2026',
+			// 	message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/rock-in-rio-2026',
+			// 	presentation_type: 'changelog',
+			// },
 		]);
 });
 
@@ -40,12 +40,12 @@ FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 				message_url: 'https://pcgi.tmlmobilidade.pt/app-navegante-ios/startup/20260127',
 				presentation_type: 'breaking',
 			},
-			{
-				build_max: null,
-				build_min: 93,
-				message_id: 'navegante-rir-2026',
-				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/rock-in-rio-2026',
-				presentation_type: 'changelog',
-			},
+			// {
+			// 	build_max: null,
+			// 	build_min: 93,
+			// 	message_id: 'navegante-rir-2026',
+			// 	message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/rock-in-rio-2026',
+			// 	presentation_type: 'changelog',
+			// },
 		]);
 });
