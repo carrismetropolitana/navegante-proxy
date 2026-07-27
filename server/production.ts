7ae1ad8ec477ab8ec58122ffbf8eb50718402ada
Samanta Coutinho <samanta.coutinho@tmlmobilidade.pt>
2026-07-27 16:30:01 +0100

    correct URL for new publicity - PROD

--- server/production.ts
+++ server/production.ts
@@ -23,7 +23,7 @@ FASTIFY.server.get('/android/v1/startup/message', async (_, reply) => {
 				build_max: null,
 				build_min: 102,
 				message_id: 'DEV-navegante-publicity-time-2026',
-				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-android/startup/dono-do-tempo',
+				message_url: 'https://pcgi.tmlmobilidade.pt/app-navegante-android/startup/dono-do-tempo',
 				presentation_type: 'changelog',
 			},
 		]);
@@ -45,7 +45,7 @@ FASTIFY.server.get('/ios/v1/startup/message', async (_, reply) => {
 				build_max: null,
 				build_min: 99,
 				message_id: 'DEV-navegante-publicity-time-2026',
-				message_url: 'https://pcgi-stg.tmlmobilidade.pt/app-navegante-ios/startup/dono-do-tempo',
+				message_url: 'https://pcgi.tmlmobilidade.pt/app-navegante-ios/startup/dono-do-tempo',
 				presentation_type: 'changelog',
 			},
 		]);
